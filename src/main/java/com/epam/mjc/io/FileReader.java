732b4f37d2cece4a2c89b88bfdb02145e98c0550
Nadejda <nadyash295923@gmail.com>
2024-04-16 16:38:29 +0300

    added reading to a string

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,25 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        int ch;
+        StringBuilder profile = new StringBuilder();
+        try (FileInputStream stream = new FileInputStream(file))
+        {
+            while ((ch = stream.read())!=-1)
+            {
+                profile.append(ch);
+            }
+            System.out.println(profile);
+        }
+        catch (IOException ee)
+        {
+            System.out.println(ee.getMessage());
+        }
         return new Profile();
     }
 }
